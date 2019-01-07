c546a11dc57a4d7c88a8ea0cccb18a587212ff33
Carlos Andrés Romero Parrado <andrestrue@hotmail.com>
2019-01-07 04:12:26 -0500

    Clientes from json file

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente} from './cliente';
+
+export const CLIENTES: Cliente[]= [
+  { id:1, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-11'},
+  { id:2, nombre: 'Mr. Jhon', apellido: 'Doe', email: 'jhon.doe@bolsadeideas.com', createAt: '2017-11-11'},
+  { id:3, nombre: 'Linux', apellido: 'Torvalds', email: 'linux.torvalds@bolsadeideas.com', createAt: '2017-11-12'},
+  { id:4, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdorf@bolsadeideas.com', createAt: '2017-11-13'},
+  { id:5, nombre: 'Erick', apellido: 'Gamma', email: 'erick.gama@bolsadeideas.com', createAt: '2017-11-14'},
+  { id:6, nombre: 'Richard', apellido: 'Helm', email: '@bolsadeideas.com', createAt: '2017-11-15'},
+  { id:7, nombre: 'Ralph', apellido: 'johnson', email: 'ralph.jhonson@bolsadeideas.com', createAt: '2017-11-16'},
+  { id:8, nombre: 'Bombasto', apellido: 'Vlissides', email: 'bombasto.blissides@bolsadeideas.com', createAt: '2017-11-17'},
+  { id:9, nombre: 'Dr James', apellido: 'Gosling', email: 'drjames.gosling@bolsadeideas.com', createAt: '2017-11-18'},
+  { id:10, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@bolsadeideas.com', createAt: '2017-11-19'},
+  { id:11, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@bolsadeideas.com', createAt: '2017-11-20'},
+]
