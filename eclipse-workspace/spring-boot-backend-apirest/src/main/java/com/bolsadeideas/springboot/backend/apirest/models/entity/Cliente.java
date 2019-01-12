168b313d608b8e0f637cab0c6f2c07661db2ebb5
Carlos Andrés Romero Parrado <andrestrue@hotmail.com>
2019-01-12 10:23:43 -0500

    BackEnd. Semilla "import.sql" + Fix cliente (nombre tabla)

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -13,7 +13,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity 
-@Table(name = "cliente")
+@Table(name = "clientes")
 public class Cliente  implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+-- IMPORTANTE. Se tiene que llamar de esta forma "import.sql" ya que SpringBoot va a buscar a traves de Hibernate si existe este archivo y lo ejecutara
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andrés','Guzmán','profesor@bolsadeideas.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mr. John','Dpe','john.doe@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Linus','Trovalds','linus.trovalds@gmail.com','2018-01-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rasmus','Lerdorf','rasmus.lerdorf@gmail.com','2018-01-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Erich','Gamma','erich.gamma@gmail.com','2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Richard','Helm','richard.helm@gmail.com','2018-02-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ralph','Johnson','ralphjohnson@gmail.com','2018-02-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('John','Vlissides','john.vlissides@gmail.com','2018-02-28');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Dr. James','Gosling','jamesgosling@gmail.com','2018-03-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Magma','Lee','magma.lee@gmail.gom','2018-03-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Tornado','Roe','tornado.roe@gmail.com','2018-03-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jade','Doe','jade.doe@gmail.com','2018-03-06');
+--INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('','','','');
