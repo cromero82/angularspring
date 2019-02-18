5f51ede4e80d50340ef0e637a6973768c051fc3e
carlos romero <soportecarlosromero@gmail.com>
2019-02-18 15:23:46 -0500

    completa consultar factura

--- angular/clientes-app/src/app/facturas/detalle-factura.component.ts
+++ angular/clientes-app/src/app/facturas/detalle-factura.component.ts
@@ -1,14 +1,25 @@
+import { FacturaService } from './services/factura.service';
 import { Component, OnInit } from '@angular/core';
+import { Factura } from './models/factura';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-factura',
   templateUrl: './detalle-factura.component.html'
 })
 export class DetalleFacturaComponent implements OnInit {
-
-  constructor() { }
+  factura: Factura
+  titulo: string = 'Factura';
+  constructor(private facturaServices: FacturaService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = +params.get('id');
+      this.facturaServices.getFactura(id).subscribe(factura => {
+        this.factura = factura;
+      })
+    })
   }
 
 }
